53097d3ba8394019a40de3d8a82ff78c2932c950
balaji-githubstore <31404824+balaji-githubstore@users.noreply.github.com>
2018-05-04 11:00:31 +0530

    Second commit firefox

--- src/test/java/com/ustglobal/test/BaseTest.java
+++ src/test/java/com/ustglobal/test/BaseTest.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 import com.ustglobal.pageobjects.HomePage;
@@ -17,7 +18,7 @@ public class BaseTest {
 	public void testMagento() throws InterruptedException {
 		// TODO Auto-generated method stub
 
-		WebDriver driver = new ChromeDriver();
+		WebDriver driver = new FirefoxDriver();
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 		driver.get("https://magento.com");
